9ab4459c2a9b75cf53153443f27d32fcf3a7bf23
Marcos Filho <sdksdk@sdksdk.local>
2017-03-06 21:59:17 -0300

    inicio do algoritmo de tempo

---
+++ src/utils/timeUtils.js
@@ -0,0 +1,11 @@
+/**
+*Script responsável pelo cálculo do horário
+**/
+import moment from 'moment';
+
+export default {
+	function calcularHorario(dataIni, dataFim, dataPausaIni, dataPausaFim) {
+
+		return '';
+	}
+}
